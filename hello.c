72d294425066be2c1be0cf07bbf9aaf55d20e141
Aditi <adititayal9@gmail.com>
2020-06-19 15:53:00 +0530

    pass argument from js to web-assembly c

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <emscripten/emscripten.h>
+
+int main(int argc, char ** argv) {
+    printf("Hello World\n");
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void EMSCRIPTEN_KEEPALIVE myFunction(int argc) {
+    printf("%d", argc);
+    printf("MyFunction Called\n");
+}
+
+#ifdef __cplusplus
+}
+#endif
